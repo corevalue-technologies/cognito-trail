a54175f12f12d93083a273d6caf151bc3e3b16a9
Ronak Dhoot <ronak.dhoot@mailsphere.co.uk>
2019-12-28 20:25:51 +0530

    ++ base spring code

---
+++ src/main/java/com/cvt/cognitotrail/CognitoTrailApplication.java
@@ -0,0 +1,13 @@
+package com.cvt.cognitotrail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CognitoTrailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CognitoTrailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cvt/cognitotrail/CognitoTrailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cvt.cognitotrail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CognitoTrailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
